9cccb43904d0bfbcc2b5f1659b826aba9d09eb3e
rmadhabachary <madhab55021@gmail.com>
2023-11-26 00:13:49 +0530

    new commits

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const btn=document.querySelectorAll('.btn')
+const btns=document.querySelectorAll('.btn')
 const body=document.querySelector('body')
 
-
+btns.forEach(function(btn){
+    btn.addEventListener('click',(e)=>{
+        if(e.target.id==="orange"){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id==="white"){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id==="green"){
+            body.style.backgroundColor=e.target.id
+        }
+    })
+})
