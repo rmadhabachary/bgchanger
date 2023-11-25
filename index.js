408d076f6d914992df095a4d02e6fab09db4a423
rmadhabachary <madhab55021@gmail.com>
2023-11-26 00:22:07 +0530

    new commit

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ btns.forEach(function(btn){
     btn.addEventListener('click',(e)=>{
         if(e.target.id==="orange"){
             body.style.backgroundColor=e.target.id
+            alert(`Are you sure to change the background color to ${e.target.id} ?`)
         }
         if(e.target.id==="white"){
             body.style.backgroundColor=e.target.id
+            alert(`Are you sure to change the background color to ${e.target.id} ?`)
         }
         if(e.target.id==="green"){
             body.style.backgroundColor=e.target.id
+            alert(`Are you sure to change the background color to ${e.target.id}?`)
         }
     })
 })
